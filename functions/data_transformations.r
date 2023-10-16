1a748af8e71bc592b1d2fdd54f27f7d8e4cf3e1d
Marte <marte.kristoffersen@student.nhh.no>
2023-10-16 07:23:07 +0200

    Edit problem 2 - forgot correct timezone

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -7,7 +7,7 @@ transform_metadata_to_df <- function(metadata){
     map(as_tibble) %>%  
     list_rbind() %>%  
     mutate(latestData = map_chr(latestData, 1, .default = "")) %>%  
-    mutate(latestData = as_datetime(latestData, tz = "Europe/Berlin")) %>% 
+    mutate(latestData = as_datetime(latestData, tz = "UTC")) %>% 
     mutate(location = map(location, unlist))%>%  
     mutate(
       lat = map_dbl(location, "latLon.lat"),
